b3fc17b260b6a3c5416bc349a848fe293464b96e
yagnesh-1811 <63344069+yagnesh-1811@users.noreply.github.com>
2022-03-05 12:17:22 +0530

    updated team models

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
     }
 }
 
@@ -122,6 +125,10 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
    BASE_DIR / 'carzone/static',
 ]
+
+
+MEDIA_ROOT= BASE_DIR / 'media'
+MEDIA_URL= '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
